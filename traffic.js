62428b4aa01b154acaaa195454ec1d7eea9db16d
keerthibalan9 <keerthiibalan9@gmail.com>
2023-10-07 19:18:47 +0530

    Traffic light button system + js + DOM

---
+++ traffic.js
@@ -0,0 +1,51 @@
+function func1(){
+    let a=document.getElementById("stop");
+    a.style.backgroundColor="#ca3120"
+    a.style.color="white"
+    let b=document.getElementById("stoplt");
+    b.style.backgroundColor="#ca3120"  
+
+    let c=document.getElementById("wait");
+    c.style.backgroundColor="#494e66"
+    let d=document.getElementById("waitlt");
+    d.style.backgroundColor="#494e66"
+    let e=document.getElementById("go");
+    e.style.backgroundColor="#494e66"
+    let f=document.getElementById("golt");
+    f.style.backgroundColor="#494e66" 
+}
+function func2(){
+    let a=document.getElementById("wait");
+  a.style.backgroundColor="#f0ad4e"
+  a.style.color="white"
+  let b=document.getElementById("waitlt");
+  b.style.backgroundColor="#f0ad4e" 
+
+  let c=document.getElementById("stop");
+  c.style.backgroundColor="#494e66"
+  let d=document.getElementById("stoplt");
+  d.style.backgroundColor="#494e66"
+  let e=document.getElementById("go");
+  e.style.backgroundColor="#494e66"
+  let f=document.getElementById("golt");
+  f.style.backgroundColor="#494e66" 
+}
+function func3(){
+    let a=document.getElementById("go");
+  a.style.backgroundColor="#11a676"
+  a.style.color="white"
+  let b=document.getElementById("golt");
+  b.style.backgroundColor="#11a676" 
+
+  let c=document.getElementById("stop");
+  c.style.backgroundColor="#494e66"
+  let d=document.getElementById("stoplt");
+  d.style.backgroundColor="#494e66"
+  let e=document.getElementById("wait");
+  e.style.backgroundColor="#494e66"
+  let f=document.getElementById("waitlt");
+  f.style.backgroundColor="#494e66" 
+}
+
+
+
