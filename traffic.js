b2863ecfab2641ab4a512e9dcc8fbf1b74bb4e52
keerthibalan9 <keerthiibalan9@gmail.com>
2023-10-08 16:51:44 +0530

    reduced lines

--- traffic.js
+++ traffic.js
@@ -1,56 +1,93 @@
-function func1(){
-    let a=document.getElementById("stop");
-    a.style.backgroundColor="#ca3120"
-    a.style.color="white"
-    let b=document.getElementById("stoplt");
-    b.style.backgroundColor="#ca3120"  
+// function func1(){
+//     let a=document.getElementById("stop");
+//     a.style.backgroundColor="#ca3120"
+//     a.style.color="white"
+//     let b=document.getElementById("stoplt");
+//     b.style.backgroundColor="#ca3120"  
 
-    let c=document.getElementById("wait");
-    c.style.backgroundColor="#ffffff"
-    c.style.color="black"
-    let d=document.getElementById("waitlt");
-    d.style.backgroundColor="#494e66"
-    let e=document.getElementById("go");
-    e.style.backgroundColor="#ffffff"
-    e.style.color="black"
-    let f=document.getElementById("golt");
-    f.style.backgroundColor="#494e66" 
+//     let c=document.getElementById("wait");
+//     c.style.backgroundColor="#ffffff"
+//     c.style.color="black"
+//     let d=document.getElementById("waitlt");
+//     d.style.backgroundColor="#494e66"
+//     let e=document.getElementById("go");
+//     e.style.backgroundColor="#ffffff"
+//     e.style.color="black"
+//     let f=document.getElementById("golt");
+//     f.style.backgroundColor="#494e66" 
+// }
+// function func2(){
+//     let a=document.getElementById("wait");
+//   a.style.backgroundColor="#f0ad4e"
+//   a.style.color="white"
+//   let b=document.getElementById("waitlt");
+//   b.style.backgroundColor="#f0ad4e" 
+
+//   let c=document.getElementById("stop");
+//   c.style.backgroundColor="#ffffff"
+//   c.style.color="black"
+//   let d=document.getElementById("stoplt");
+//   d.style.backgroundColor="#494e66"
+//   let e=document.getElementById("go");
+//   e.style.backgroundColor="#ffffff"
+//   e.style.color="black"
+//   let f=document.getElementById("golt");
+//   f.style.backgroundColor="#494e66" 
+// }
+// function func3(){
+//     let a=document.getElementById("go");
+//   a.style.backgroundColor="#11a676"
+//   a.style.color="white"
+//   let b=document.getElementById("golt");
+//   b.style.backgroundColor="#11a676" 
+
+//   let c=document.getElementById("stop");
+//   c.style.backgroundColor="#ffffff"
+//   c.style.color="black"
+//   let d=document.getElementById("stoplt");
+//   d.style.backgroundColor="#494e66"
+//   let e=document.getElementById("wait");
+//   e.style.backgroundColor="#ffffff"
+//   e.style.color="black"
+//   let f=document.getElementById("waitlt");
+//   f.style.backgroundColor="#494e66" 
+// }
+
+
+
+function func1() {
+  document.getElementById("stop").style.backgroundColor = "#ca3120"
+  document.getElementById("stop").style.color = "white"
+  document.getElementById("stoplt").style.backgroundColor = "#ca3120"
+  document.getElementById("wait").style.backgroundColor = "#ffffff"
+  document.getElementById("wait").style.color = "black"
+  document.getElementById("waitlt").style.backgroundColor = "#494e66"
+  document.getElementById("go").style.backgroundColor = "#ffffff"
+  document.getElementById("go").style.color = "black"
+  document.getElementById("golt").style.backgroundColor = "#494e66"
 }
-function func2(){
-    let a=document.getElementById("wait");
-  a.style.backgroundColor="#f0ad4e"
-  a.style.color="white"
-  let b=document.getElementById("waitlt");
-  b.style.backgroundColor="#f0ad4e" 
-
-  let c=document.getElementById("stop");
-  c.style.backgroundColor="#ffffff"
-  c.style.color="black"
-  let d=document.getElementById("stoplt");
-  d.style.backgroundColor="#494e66"
-  let e=document.getElementById("go");
-  e.style.backgroundColor="#ffffff"
-  e.style.color="black"
-  let f=document.getElementById("golt");
-  f.style.backgroundColor="#494e66" 
+function func2() {
+  document.getElementById("wait").style.backgroundColor = "#f0ad4e"
+  document.getElementById("wait").style.color = "white"
+  document.getElementById("waitlt").style.backgroundColor = "#f0ad4e"
+  document.getElementById("stop").style.backgroundColor = "#ffffff"
+  document.getElementById("stop").style.color = "black"
+  document.getElementById("stoplt").style.backgroundColor = "#494e66"
+  document.getElementById("go").style.backgroundColor = "#ffffff"
+  document.getElementById("go").style.color = "black"
+  document.getElementById("golt").style.backgroundColor = "#494e66"
 }
-function func3(){
-    let a=document.getElementById("go");
-  a.style.backgroundColor="#11a676"
-  a.style.color="white"
-  let b=document.getElementById("golt");
-  b.style.backgroundColor="#11a676" 
+function func3() {
+  document.getElementById("go").style.backgroundColor = "#11a676"
+  document.getElementById("go").style.color = "white"
+  document.getElementById("golt").style.backgroundColor = "#11a676"
+  document.getElementById("stop").style.backgroundColor = "#ffffff"
+  document.getElementById("stop").style.color = "black"
+  document.getElementById("stoplt").style.backgroundColor = "#494e66"
+  document.getElementById("wait").style.backgroundColor = "#ffffff"
+  document.getElementById("wait").style.color = "black"
+  document.getElementById("waitlt").style.backgroundColor = "#494e66"
 
-  let c=document.getElementById("stop");
-  c.style.backgroundColor="#ffffff"
-  c.style.color="black"
-  let d=document.getElementById("stoplt");
-  d.style.backgroundColor="#494e66"
-  let e=document.getElementById("wait");
-  e.style.backgroundColor="#ffffff"
-  e.style.color="black"
-  let f=document.getElementById("waitlt");
-  f.style.backgroundColor="#494e66" 
 }
 
 
